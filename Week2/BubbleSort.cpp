be4721ceb6074ff7045eb007536ff449f91eef86
KuuhakuCNK <24520727@gm.uit.edu.vn>
2025-03-27 13:06:02 +0700

    bubble sort

--- Week2/BubbleSort.cpp
+++ Week2/BubbleSort.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-void swapp(int &a, int &b){
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
 // Độ phức tạp O(n^2)
-void BubbleSort(vector <int> &a, int n){
+void BubbleSort(vector <int> &a){
+    int n = a.size();
     bool swapped;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         // phần tử swapped này để check xem coi mảng đã được sort hết chưa
         swapped = false;
@@ -16,13 +12,13 @@ void BubbleSort(vector <int> &a, int n){
         {
             // Đổi phần tử thứ j với phần tử tiếp theo là j + 1 nếu giá trị lớn hơn
             if (a[j] > a[j+1]){
-                swapp(a[j],a[j+1]);
+                swap(a[j],a[j+1]);
                 swapped = true;
+                for (auto x : a){
+                    cout << x << " ";
+                }
+                cout << endl;
             }
-            for (auto x : a){
-                cout << x << " ";
-            }
-            cout << endl;
         }
         // Nếu mảng ko đc swap lần nào nữa thì có nghĩ là nó đã sort xong
         if (!swapped){
@@ -44,5 +40,5 @@ int main(){
         cin >> x;
         a.push_back(x);
     }
-    BubbleSort(a,n);
+    BubbleSort(a);
 }
