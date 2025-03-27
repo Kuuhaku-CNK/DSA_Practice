1398957639356b257c5a10939fedb2aa65561ad8
KuuhakuCNK <24520727@gm.uit.edu.vn>
2025-03-27 15:36:06 +0700

    tronmang

--- Week2/tempCodeRunnerFile.cpp
+++ Week2/tempCodeRunnerFile.cpp
@@ -1,39 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
-// Độ phức tạp O(n^2)
-void InsertionSort(vector <int> &a){
-    int n = a.size();
-    for (int i = 1; i < n; i++)
-    {
-        int key = a[i];
-        int j = i - 1;
-        // Điều kiện là j lớn hơn phần tử key và chưa chạy về đầu mảng
-        while (j >= 0 && a[j] > key){
-            // Dời vị trí của phần tửtrước đó lên 1 vị trí
-            a[j+1] = a[j];
-            j--;
-            for (auto x : a){
-                cout << x << " ";
-            }
-            cout << '\n';
+void merge(int a[],int n,int b[],int m,int c[]){
+    int i = 0, j = 0, k = 0;
+    while (i < n && j < m){
+        if (a[i] < b[j]){
+            c[k] = a[i];
+            i++;
+            k++;
+        } else {
+            c[k] = b[j];
+            j++;
+            k++;
         }
-        // Gán phần tử 
-        a[j+1] = key;
     }
-}
-int main(){
-    freopen("insertionsort.inp","r",stdin);
-    freopen("insertionsort.out","w",stdout);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin >> n;
-    vector <int> a;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        a.push_back(x);
+    int tmp1 = n - i;
+    int tmp2 = m - j;
+    if (tmp1 != 0){
+        while (i < n){
+            c[k] = a[i];
+            k++;
+            i++;
+        }
     }
-    InsertionSort(a);
+    if (tmp2 != 0){
+        while (j < n){
+            c[k] = b[j];
+            k++;
+            j++;
+        }
+    }
 }
