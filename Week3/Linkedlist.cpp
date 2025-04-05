1be51e06d2e2f91b1dd2e5e36f847b8489f04fb2
KuuhakuCNK <24520727@gm.uit.edu.vn>
2025-04-05 11:10:40 +0700

    linked list

--- Week3/Linkedlist.cpp
+++ Week3/Linkedlist.cpp
@@ -2,19 +2,32 @@
 using namespace std;
 struct node
 {
-    int data;
+    int value;
     struct node *next;
-};
-struct TNODE{
-    int key;
-    TNODE* pLeft;
-    TNODE* pRight;
 };
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    struct node *head;
-    struct node *one = NULL;
-    struct node *two = NULL;
-    struct node *three = NULL;
+    node *head;
+    node *one = NULL;
+    node *two = NULL;
+    node *three = NULL;
+    // allocate 3 nodes in heap
+    one = new node();
+    two = new node();
+    three = new node();
+    // assign value
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
+    // connect nodes
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+    head = one;
+    // print the linkedlist
+    while (head != NULL){
+        cout << head->value << endl;
+        head = head->next;
+    }
 }
